62c34118cea93265bf3bbf886a83d187f604340b
Kuwana0425 <y.kuwana.cingroup2012@gmail.com>
2024-09-04 16:56:42 +0900

    お問い合わせフォーム実装 その3 再コミット&プッシュ

--- practice/src/main/java/com/example/demo/repository/ContactRepository.java
+++ practice/src/main/java/com/example/demo/repository/ContactRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-
 }
